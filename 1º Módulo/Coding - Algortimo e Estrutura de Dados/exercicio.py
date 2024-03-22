2208254ad780caddb1795924beaaeed1dbae2cdb
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2024-03-22 20:44:32 -0300

    aula de algoritmo

---
+++ 1º Módulo/Coding - Algortimo e Estrutura de Dados/exercicio.py
@@ -0,0 +1,21 @@
+print("Hello, Word!")
+
+nome = "Laércio"
+print("Olá, meu nome é", nome)
+
+idade = 26
+print(idade)
+
+pessoa = "Laércio"
+idade = 26
+altura = 1.71
+desenvolvedor = True 
+
+type(altura)
+
+
+
+
+
+
+
