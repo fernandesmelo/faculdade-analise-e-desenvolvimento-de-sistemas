b1e0f2e947e3469c717d18889d93a06cedb34e98
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2024-03-26 10:36:33 -0300

    organiza pastas

---
+++ 1º Módulo/Disciplina Unidades de Extensão - Front End/Semana 01 - Lógica de Programação - mergulhe em programação com JavaScript/Jogo de Número Secreto/app.js
@@ -0,0 +1,7 @@
+alert("Bem-vindo ao jogo de número secreto!");
+let numeroSecreto = 29;
+let chute = prompt("Escolha um número entre 1 e 30")
+
+if (numeroSecreto == chute) {
+    console.log("Isso aí! Você descobriu o número secreto.");
+}
